ffd3535e9b22e51fc2a249c816755196f8d8b462
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-17 14:37:14 +0800

    Update geolocation.controller.js

--- geolocation.controller.js
+++ geolocation.controller.js
@@ -3,23 +3,28 @@ app.controller("g",geolocationController);
 function geolocationController( $scope, $http)
 {
 	var vm=this;
-	alert(navigator.geolocation);
-        if (navigator.geolocation) {
-            navigator.geolocation.watchPosition(function(position) {
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-		    vm.lat=position.coords.latitude;
-		    vm.lng=position.coords.longitude;
-            }, function() {
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-        } else {
-            // Browser doesn't support Geolocation
+	
+	vm.cc=function (){
+		if (navigator.geolocation) {
+		    navigator.geolocation.watchPosition(function(position) {
+			var pos = {
+			    lat: position.coords.latitude,
+			    lng: position.coords.longitude
+			};
+			    vm.lat=position.coords.latitude;
+			    vm.lng=position.coords.longitude;
+			    alert (vm.lat);
+			    vm.cc();
+		    }, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		    });
+		} else {
+		    // Browser doesn't support Geolocation
 
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
+		    handleLocationError(false, infoWindow, map.getCenter());
+		}
+	}
+	vm.cc();
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(browserHasGeolocation ?
