0b9f47cdaace9023e1c7e2e627a151b903722f28
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-17 17:09:11 +0800

    Update testapp.js

--- testapp.js
+++ testapp.js
@@ -16,6 +16,6 @@ angular
       enableHighAccuracy: true
     });
     $scope.coords = $geolocation.position.coords; // this is regularly updated
-    vm.aa= $scope.coords.latitude;
+    vm.aa= $scope.coords;
     $scope.error = $geolocation.position.error; // this becomes truthy, and has 'code' and 'message' if an error occurs
   });
