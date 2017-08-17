04d0bffd3e281703cc47fbe04f29bd9b712665df
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-17 13:53:29 +0800

    Update geolocation.controller.js

--- geolocation.controller.js
+++ geolocation.controller.js
@@ -3,6 +3,16 @@ app.controller("g",geolocationController);
 function geolocationController($scope,$http)
 {
 	var vm=this;
+	var mapOptions = {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
        $scope.loadMap = function() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
