13e37a48e8fb1895be196653a2a90ddce0780f51
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-17 13:45:52 +0800

    Update geolocation.controller.js

--- geolocation.controller.js
+++ geolocation.controller.js
@@ -3,7 +3,30 @@ app.controller("g",geolocationController);
 function geolocationController($scope,$http)
 {
 	var vm=this;
-       vm.test=position.coords.accuracy;;
-		
+       $scope.loadMap = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                $scope.map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    };
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+    }
+}
 			
 }
