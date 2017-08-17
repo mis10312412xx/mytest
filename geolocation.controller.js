c1d438e84cee9e755181d366396ffbb6d917cfb0
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-17 14:31:41 +0800

    Update geolocation.controller.js

--- geolocation.controller.js
+++ geolocation.controller.js
@@ -11,8 +11,6 @@ function geolocationController( $scope, $http)
                 };
 		    vm.lat=position.coords.latitude;
 		    vm.lng=position.coords.longitude;
-                    alert (vm.lat);
-		    alert (vm.lng);
             }, function() {
                 handleLocationError(true, infoWindow, map.getCenter());
             });
