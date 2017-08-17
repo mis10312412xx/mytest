74b338bbc175e5a359a807450a169d1f81ceb27c
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-17 12:31:39 +0800

    Update geolocation.controller.js

--- geolocation.controller.js
+++ geolocation.controller.js
@@ -1,5 +1,5 @@
-angular.module('app',[]);
-controller("g",geolocationController);
+var app=angular.module('app',[]);
+app.controller("g",geolocationController);
 function geolocationController($scope,$http)
 {
 	var vm=this;
