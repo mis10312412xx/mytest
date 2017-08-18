460e322be04a5800249957dd0244698af7f5012a
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-18 09:21:21 +0800

    Update geolaction.js

--- geolaction.js
+++ geolaction.js
@@ -3,18 +3,23 @@
 // Code goes here
   
 var myApp = angular.module('myApp',['geolocation']);
-myApp.controller('mainCtrl', function ($scope,geolocation,$interval) {
+myApp.controller('mainCtrl', function ($scope,geolocation,$interval,$window) {
   /*  $scope.coords = geolocation.getLocation().then(function(data){
       return {lat:data.coords.latitude, long:data.coords.longitude};
     });*/
+    $scope.stolat=22.6395202;
+    $scope.stolat=120.3215537;
+    $scope.range=0;
     $scope.coords = geolocation.getLocation().then(function(data){
       $scope.lat=data.coords.latitude; $scope.lng=data.coords.longitude;
+      $scope.range=$window.Math.sqrt(($scope.stolat-$scope.lat)*($scope.stolat-$scope.lat)-($scope.stolng-$scope.lng)*($scope.stolng-$scope.lng));
       return {lat:data.coords.latitude, long:data.coords.longitude};
     });
     $scope.toDo = toDO;
      function toDO(){
      $scope.coords = geolocation.getLocation().then(function(data){
        $scope.lat=data.coords.latitude; $scope.lng=data.coords.longitude;
+             $scope.range=$window.Math.sqrt(($scope.stolat-$scope.lat)*($scope.stolat-$scope.lat)-($scope.stolng-$scope.lng)*($scope.stolng-$scope.lng));
       return {lat:data.coords.latitude, long:data.coords.longitude};
     });
      console.log($scope.lat);
