70d9cea74158b4d59f12403c11b7138c1ac5cc9d
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-18 08:11:47 +0800

    Update geolaction.js

--- geolaction.js
+++ geolaction.js
@@ -3,9 +3,20 @@
 // Code goes here
 
 var myApp = angular.module('myApp',['geolocation']);
-myApp.controller('mainCtrl', function ($scope,geolocation) {
+myApp.controller('mainCtrl', function ($scope,geolocation,$timeout) {
     $scope.coords = geolocation.getLocation().then(function(data){
       return {lat:data.coords.latitude, long:data.coords.longitude};
     });
+    var toDo = function {
+     $scope.newcoords = geolocation.getLocation().then(function(data){
+      return {lat:data.coords.latitude, long:data.coords.longitude};
+    });
+     console.log($scope.newcoords);
+
+};
+$timeout(toDo,5000)
+
+//原文網址：https://read01.com/RnjDyG.html
+  
 });
 
