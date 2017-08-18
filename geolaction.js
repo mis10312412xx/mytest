2ad15e11b4189406b36e9cadbd90ebce2679ba66
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-18 09:10:22 +0800

    Update geolaction.js

--- geolaction.js
+++ geolaction.js
@@ -8,15 +8,17 @@ myApp.controller('mainCtrl', function ($scope,geolocation,$interval) {
       return {lat:data.coords.latitude, long:data.coords.longitude};
     });*/
     $scope.coords = geolocation.getLocation().then(function(data){
+      $scope.lat=data.coords.latitude; $scope.lng=data.coords.longitude;
       return {lat:data.coords.latitude, long:data.coords.longitude};
     });
     $scope.toDo = toDO;
      function toDO(){
      $scope.coords = geolocation.getLocation().then(function(data){
+       $scope.lat=data.coords.latitude; $scope.lng=data.coords.longitude;
       return {lat:data.coords.latitude, long:data.coords.longitude};
     });
-     console.log($scope.coords);
-      
+     console.log($scope.lat);
+     console.log($scope.lng);
     };
    $interval(toDO,1000);
 });
