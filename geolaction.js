a2a60d4c41253fcc5b1a581abde2b53d5e35c02a
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-18 08:32:23 +0800

    Update geolaction.js

--- geolaction.js
+++ geolaction.js
@@ -1,26 +1,20 @@
 
   'use strict';
 // Code goes here
-
+  
 var myApp = angular.module('myApp',['geolocation']);
 myApp.controller('mainCtrl', function ($scope,geolocation,$timeout) {
     $scope.coords = geolocation.getLocation().then(function(data){
       return {lat:data.coords.latitude, long:data.coords.longitude};
     });
+    toDO;
     var toDo = function (){
      $scope.newcoords = geolocation.getLocation().then(function(data){
       return {lat:data.coords.latitude, long:data.coords.longitude};
     });
      console.log($scope.newcoords);
-};
-$timeout(toDo,1000)
-var totest = function () {
-
-console.log("Hello World");
-
-};
-
-$timeout(totest,100)
-  
+      toDO;
+    };
+     
 });
 
