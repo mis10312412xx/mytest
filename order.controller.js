60cfb8a6c1da2923fa2ca0d8946d22991435acf4
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-29 14:46:43 +0800

    Create order.controller.js

---
+++ order.controller.js
@@ -0,0 +1,45 @@
+var app=angular.module('app',[]);
+app.controller("order",order);
+function order( $scope, $http)
+{
+	var vm=this;
+	vm.addDishes=addDishes;
+	vm.deleteDishes=deleteDishes;
+	vm.saveDishes=saveDishes;
+	vm.menudata=[];
+	vm.temp=[];
+	vm.savestate="y";
+	vm.menudata.push({
+		name: "",
+		price: ""
+	});
+	function addDishes(){
+		vm.menudata.push({
+		name: "",
+		price: ""
+		});
+	}
+	function deleteDishes(index){
+		vm.temp=[];
+	     for(i in vm.menudata){
+		if(i!=index){
+		   vm.temp.push(vm.menudata[i]);
+		}
+	     }
+		vm.menudata=vm.temp;
+	}
+	function saveDishes(){
+	  for(i in vm.menudata){
+		if(vm.menudata[i].name=="" || vm.menudata[i].price==""){
+		     vm.savestate="n";
+		     alert("有欄位還沒填");
+		}
+	     }
+		if(vm.savestate=="y"){
+		 	alert("儲存成功");
+		}else{
+			vm.savestate="y";
+		}
+		
+	}
+}
