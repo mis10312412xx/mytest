e3f4270d5b9930344bd573e19605f81e06ea586f
mis10312412xx <31083831+mis10312412xx@users.noreply.github.com>
2017-08-29 09:29:05 +0800

    Update makemenu.controller.js

--- makemenu.controller.js
+++ makemenu.controller.js
@@ -1,30 +1,8 @@
 var app=angular.module('app',[]);
-app.controller("g",geolocationController);
-function geolocationController( $scope, $http)
+app.controller("makemenu",makemenu);
+function makemenu( $scope, $http)
 {
 	var vm=this;
-		if (navigator.geolocation) {
-		    navigator.geolocation.watchPosition(function(position) {
-			var pos = {
-			    lat: position.coords.latitude,
-			    lng: position.coords.longitude
-			};
-			    vm.lat=position.coords.latitude;
-			    vm.lng=position.coords.longitude;
-			   
-		    }, function() {
-			handleLocationError(true, infoWindow, map.getCenter());
-		    });
-		} else {
-		    // Browser doesn't support Geolocation
-
-		    handleLocationError(false, infoWindow, map.getCenter());
-		}
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
-    }
+	
 	
 }
